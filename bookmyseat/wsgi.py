1b19f96922c4230c47b2096e5d499f18140e72b9
Koushikdama <147995467+Koushikdama@users.noreply.github.com>
2024-12-19 15:38:44 +0530

    Update wsgi.py

--- bookmyseat/wsgi.py
+++ bookmyseat/wsgi.py
@@ -7,15 +7,23 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+# from django.core.wsgi import get_wsgi_application
+# from django.contrib.staticfiles.handlers import StaticFilesHandler
+
+# application = StaticFilesHandler(get_wsgi_application())
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookmyseat.settings')
+
+# application = get_wsgi_application()
+# app = application
+
+
 import os
-
 from django.core.wsgi import get_wsgi_application
-from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-
-application = StaticFilesHandler(get_wsgi_application())
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookmyseat.settings')
 
 application = get_wsgi_application()
-app = application
