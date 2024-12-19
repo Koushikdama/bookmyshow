84c27c704b1b982ed71fbf0dd7a351286d07b306
Koushikdama <147995467+Koushikdama@users.noreply.github.com>
2024-12-19 15:40:14 +0530

    Update wsgi.py

--- bookmyseat/wsgi.py
+++ bookmyseat/wsgi.py
@@ -27,3 +27,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bookmyseat.settings')
 
 application = get_wsgi_application()
+
+# Vercel requires a variable named `app` or `handler`
+app = application
+
