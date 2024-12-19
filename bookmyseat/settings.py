66d5bc3a7834e4cd59bc74bf0ac0ed1cb58eb3e5
Koushikdama <147995467+Koushikdama@users.noreply.github.com>
2024-12-19 15:46:10 +0530

    Update settings.py

--- bookmyseat/settings.py
+++ bookmyseat/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+      'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 AUTH_USER_MODEL='auth.User'
